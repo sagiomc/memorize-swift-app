7f4c54643aeebb6a47aed462e76a34610c7da27f
Giovanni Santiago <christian.santiago@netlogistik.com>
2020-09-12 18:05:16 -0500

    lecture 2: mvvm and the swift type system

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Christian Santiago on 12/09/20.
+//  Copyright © 2020 Hades. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+    
+}

1b04632682f16efd15d0ff3b83d45fd222ff679d
Giovanni Santiago <christian.santiago@netlogistik.com>
2020-09-12 11:58:44 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Christian Santiago on 12/09/20.
+//  Copyright © 2020 Hades. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
